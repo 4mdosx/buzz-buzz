c7696c3bd1dbbdb46578f83445e4453e3c4edf66
syntachaosx <chaosdog.tl@gmail.com>
2020-06-19 21:15:59 +0800

    :fire:

--- db/migrations/1592141206580-create_users.ts
+++ db/migrations/1592141206580-create_users.ts
@@ -2,7 +2,6 @@ import { Migration } from 'https://deno.land/x/nessie/mod.ts'
 import { Schema } from 'https://deno.land/x/nessie/qb.ts'
 
 export const up: Migration<Schema> = ({ queryBuilder }) => {
-  console.log(queryBuilder)
   queryBuilder.create('users', (table) => {
     table.id()
     table.string('username', 100).nullable().unique()

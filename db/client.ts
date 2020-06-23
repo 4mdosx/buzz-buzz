7acf595b40831654abf559308cdac061d6ad20b2
syntachaosx <chaosdog.tl@gmail.com>
2020-06-23 17:03:56 +0800

    :sparkles:  update config for  dex queryBuild debug

--- db/client.ts
+++ db/client.ts
@@ -1,17 +1,23 @@
-import { Pool } from "../deps.ts";
+import { Pool } from '../deps.ts'
 import config from '../config.js'
 import Dex from 'https://deno.land/x/dex/mod.ts'
 
 export const dex = Dex({ client: 'postgres' })
-
-async function initPool () {
+async function initPool() {
   const pool = new Pool(config.pg as any)
+  const debug = config.dexDebug
 
   await pool.connect()
   return {
-    query () {
+    query() {
       const build = dex.queryBuilder()
-      build.fetch = async () => await pool.query(build.toString())
+      build.fetch = async () => {
+        const queryString = build.toString()
+        if (debug) {
+          console.log(queryString)
+        }
+        return await pool.query(queryString)
+      }
       return build
     }
   }
@@ -19,6 +25,6 @@ async function initPool () {
 
 const pool = await initPool()
 
-export function getPool () {
+export function getPool() {
   return pool
 }
